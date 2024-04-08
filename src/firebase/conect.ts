f56477982a444430b41faeb73fba3e83ec2aebba
Zhihare <vladislav.zhihar@gmail.com>
2024-04-08 15:26:59 +0300

    fix key

--- src/firebase/conect.ts
+++ src/firebase/conect.ts
@@ -6,14 +6,14 @@ import { getDatabase } from 'firebase/database';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNMHDSnAG5pDBy_Y66rSicSHIcP1nJ_Hs",
-  authDomain: "polyglot-3a4e2.firebaseapp.com",
-  databaseURL: "https://polyglot-3a4e2-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "polyglot-3a4e2",
-  storageBucket: "polyglot-3a4e2.appspot.com",
-  messagingSenderId: "471466554032",
-  appId: "1:471466554032:web:30d670aa688448d40f0175",
-  measurementId: "G-XRWJQP4GQV"
+  apiKey: process.env.REACT_APP_FB_APIKEY,
+  authDomain: process.env.REACT_APP_FB_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_FB_DATABASE_URL,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE,
+  messagingSenderId: process.env.REACT_APP_FB_MSIS,
+  appId: process.env.REACT_APP_FB_APP_ID,
+  measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID,
 };
 
 
