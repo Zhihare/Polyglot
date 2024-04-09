56d1f45563d26fb04b52c3fbaa603794a4d102a3
Zhihare <vladislav.zhihar@gmail.com>
2024-04-09 12:08:10 +0300

    connect

--- src/firebase/conect.ts
+++ src/firebase/conect.ts
@@ -5,6 +5,18 @@ import { getDatabase } from 'firebase/database';
 
 
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCNMHDSnAG5pDBy_Y66rSicSHIcP1nJ_Hs",
+//   authDomain: "polyglot-3a4e2.firebaseapp.com",
+//   databaseURL: "https://polyglot-3a4e2-default-rtdb.europe-west1.firebasedatabase.app",
+//   projectId: "polyglot-3a4e2",
+//   storageBucket: "polyglot-3a4e2.appspot.com",
+//   messagingSenderId: "471466554032",
+//   appId: "1:471466554032:web:30d670aa688448d40f0175",
+//   measurementId: "G-XRWJQP4GQV"
+// };
+
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FB_APIKEY,
   authDomain: process.env.REACT_APP_FB_AUTHDOMAIN,
@@ -13,7 +25,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FB_STORAGE,
   messagingSenderId: process.env.REACT_APP_FB_MSIS,
   appId: process.env.REACT_APP_FB_APP_ID,
-  measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID,
+  measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID
 };
 
 
