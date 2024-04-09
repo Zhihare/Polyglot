293794d04e9a03ad7942854ebde81415ebe1219b
Zhihare <vladislav.zhihar@gmail.com>
2024-04-09 12:08:54 +0300

    Update conect.ts

--- src/firebase/conect.ts
+++ src/firebase/conect.ts
@@ -5,18 +5,6 @@ import { getDatabase } from 'firebase/database';
 
 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCNMHDSnAG5pDBy_Y66rSicSHIcP1nJ_Hs",
-//   authDomain: "polyglot-3a4e2.firebaseapp.com",
-//   databaseURL: "https://polyglot-3a4e2-default-rtdb.europe-west1.firebasedatabase.app",
-//   projectId: "polyglot-3a4e2",
-//   storageBucket: "polyglot-3a4e2.appspot.com",
-//   messagingSenderId: "471466554032",
-//   appId: "1:471466554032:web:30d670aa688448d40f0175",
-//   measurementId: "G-XRWJQP4GQV"
-// };
-
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FB_APIKEY,
   authDomain: process.env.REACT_APP_FB_AUTHDOMAIN,
